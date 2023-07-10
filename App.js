1f26ed370517b5f9f7baaf77767999d8154867df
An T.G <boltcarrier@naver.com>
2023-07-10 15:26:54 +0900

    계산기 로직 구현에 필요한 state 셋팅

--- App.js
+++ App.js
@@ -1,7 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [input, setInput] = useState(0); // 계산기에 입력한 값
+  const [currentOperator, setCurrentOperator] = useState(null); // 현재 선택된 연산자
+  const [result, setResult] = useState(null); // 연산 결과 값
+  const [tempInput, setTempInput] = useState(null); // = 클릭 시 직전 입력한 값
+  const [tempOperator, setTempOperator] = useState(null); // = 클릭 시 직접 입력한 연산자
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -13,8 +20,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,7 @@
+# 계산기 (Mac OS 계산기 UI)
+
+## 커맨드
+
+## 산출물 이미지
+
+![result-screenshot](./result-screenshot.png)
