696519fff731de5b82c26c4a45178379d2a93230
An T.G <boltcarrier@naver.com>
2023-07-10 18:05:02 +0900

    계산 및 초기화 로직 예외처리

--- src/Calculator.js
+++ src/Calculator.js
@@ -57,11 +57,17 @@ export default () => {
   const [result, setResult] = useState(null); // 연산 결과 값
   const [tempInput, setTempInput] = useState(null); // = 클릭 시 직전 입력한 값
   const [tempOperator, setTempOperator] = useState(null); // = 클릭 시 직접 입력한 연산자
+  const [isClickedOperator, setIsClickedOperator] = useState(false);
+  const [isClickedEqual, setIsClickedEqual] = useState(false); // = 클릭 여부
+
+  // const hasInput = input ? true : false;
+  const hasInput = !!input;
 
   const onPressNum = (num) => {
-    if (currentOperator) {
+    if (currentOperator && isClickedOperator) {
       setResult(input);
       setInput(num);
+      setIsClickedOperator(false);
     } else {
       const newInput = Number(`${input}${num}`); // 0으로 시작하는 경우 제외
       setInput(newInput);
@@ -71,35 +77,47 @@ export default () => {
   const onPressOperator = (operator) => {
     if (operator !== "=") {
       setCurrentOperator(operator);
+      setIsClickedOperator(true);
+      setIsClickedEqual(false);
     } else {
       let finalResult = result;
-      switch (currentOperator) {
+      const finalInput = isClickedEqual ? tempInput : input;
+      const finalOperator = isClickedEqual ? tempOperator : currentOperator;
+      switch (finalOperator) {
         case "+":
-          finalResult = result + input;
+          finalResult = result + finalInput;
           break;
         case "-":
-          finalResult = result - input;
+          finalResult = result - finalInput;
           break;
         case "*":
-          finalResult = result * input;
+          finalResult = result * finalInput;
           break;
         case "/":
-          finalResult = result / input;
+          finalResult = result / finalInput;
           break;
         default:
           break;
       }
       setResult(finalResult);
       setInput(finalResult);
+      setTempInput(finalInput);
+      setCurrentOperator(null);
+      setTempOperator(finalOperator);
+      setIsClickedEqual(true);
     }
   };
 
   const onPressReset = () => {
-    setInput(0);
-    setCurrentOperator(null);
-    setResult(null);
-    setTempInput(null);
-    setTempOperator(null);
+    if (hasInput) {
+      setInput(0);
+    } else {
+      setInput(0);
+      setCurrentOperator(null);
+      setResult(null);
+      setTempInput(null);
+      setTempOperator(null);
+    }
   };
 
   return (
@@ -120,7 +138,7 @@ export default () => {
       <ButtonContainer>
         <Button
           type="reset"
-          text="AC"
+          text={hasInput ? "C" : "AC"}
           onPress={onPressReset} // {() => onPressReset()} 와 동일
           flex={3}
         />
